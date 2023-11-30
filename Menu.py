d51fdad93a9af5042d73663775ad2ee92cfecda6
hopepx <hopeparadoxsolos@gmail.com>
2023-12-01 00:54:10 +0800

    [Add] Main Menu

---
+++ Menu.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self, name, score, coins):
+        self.name = name
+        self.score = score
+        self.coins = coins
+        
+    def printName(self):
+        print("Name: {}".format(self.name))
+        
+
+
+
+
