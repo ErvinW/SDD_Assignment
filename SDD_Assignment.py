40cef7f05e8d77a7ed226cb3810ea77e0aaeb1bb
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-12-02 00:42:27 +0800

    Functions to initialise new game and go through turns

--- SDD_Assignment.py
+++ SDD_Assignment.py
@@ -1,5 +1,6 @@
 ##SDD Assignment 
 ## Done by Ervin, Xin Yin, Xue Wen, Keene, Bi De
+import random
 from User import User
 
 
@@ -26,14 +27,48 @@ field = [[[None,None],[None,None],[None,None],[None,None],[None,None],[None,None
     ]
 
 letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T']
-      
 
+playerName = ""
+
+
+
+
+
+
+
+def init_turn(turn, score, coins):
+    while True:
+     turn += 1 
+     buildingOne = random.randint(0, 4)
+     buildingTwo = random.randint(0, 4)
+     buildingList = [buildingOne, buildingTwo]
+     draw_map()
+     print("Turn: {} {:>10}{:>10}".format(str(turn), str(score), str(coins)))
+     print("Options:")
+     print("{:20}{:<20}{:>20}".format("1. Build", "2. Save and Exit", "3. Quit without saving"))
+     action = input("Enter an action")
+     if action == "1":
+         print("Build")
+        
+     elif action == "2":
+         print("game saved")
+         return
+     
+     elif action == "3":
+         print("Exited")
+         break
+     else:
+         print("Next turn")
+         init_turn(turn, score+3, coins-1)
+     
+     
+
+     
 def draw_map():
     borders = "+-----"
     end = "+"
     rows = len(field)
     columns = len(field[0])
-    #print("{:>6}{:>6}{:>6}".format("1","2","3"))
     for i in range(0, 19):
         print("{:>6}".format(i), end="")
     print("{:>6}".format("19"))
@@ -77,6 +112,9 @@ def draw_map():
 
 def show_main_menu():
     while True:
+        score = 0 
+        coins = 16
+        turn = 0
         print()
         print("----------------")
         print("| Ngee Ann City |")
@@ -91,7 +129,7 @@ def show_main_menu():
         4. Exit")
         option = input("Enter your choice: ")
         if option == '1':
-                draw_map()
+                init_turn(turn, score, coins)
                 
             
 
@@ -105,6 +143,7 @@ def show_main_menu():
     
         elif option == '4':
                 print("Goodbye")
+                return False
             
     
         else:
@@ -114,4 +153,7 @@ def show_main_menu():
         
   
             
+
+
+
 show_main_menu()
