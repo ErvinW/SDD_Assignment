8ea59cf68e6bcd618f3565bbcfe4846b1ef7ab2e
Xin Yin <s10243745@connect.np.edu.sg>
2023-12-31 20:54:33 +0800

    Create function to calculate score - Display score - Fix error buildings must be built orthogonally adjacent to existing buildings

--- SDD_Assignment.py
+++ SDD_Assignment.py
@@ -31,7 +31,8 @@ field = [[[None,None],[None,None],[None,None],[None,None],[None,None],[None,None
 letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T']
 
 playerName = ""
-
+#score = 0
+#coins = 16
 
 class Building:
     def __init__(self):
@@ -39,7 +40,10 @@ class Building:
         self.row = None
         self.col = None
 
-
+class Game:
+    def __init__(self):
+        self.coins = 16
+        self.score = 0
 
 
 def init_turn(turn, score, coins):
@@ -50,20 +54,24 @@ def init_turn(turn, score, coins):
      random2building = random.sample(buildingList, 2)
 
      draw_map()
-     print("Turn: {} {:>10}{:>10}".format(str(turn), str(score), str(coins)))
+     print("Turn: {:<10} Score: {:<10}  Coins: {:<10}".format(str(turn), str(score), str(coins)))
      print("Options:")
      print("{:20}{:<20}{:>20}".format("1. Build", "2. Save and Exit", "3. Quit without saving"))
      action = input("Enter an action: ")
      if action == "1":
          print("Build")
-         choice = input("Choose the building to build ({}/{})".format(random2building[0],random2building[1])).upper()
+         choice = input("Choose the building to build ({}/{}): ".format(random2building[0],random2building[1])).upper()
          if choice not in buildingList:
              print("Invalid building type.")
              turn -= 1
              return
          else:
              coins -=1
-             build(turn, choice)
+             print("Points: "+ str(score))
+             btype, row, col = build(turn, choice)
+             score_to_add, coins_to_add = calculateScore(btype, row, col)
+             score += score_to_add
+             coins += coins_to_add
         
      elif action == "2":
          print("game saved")
@@ -146,9 +154,6 @@ def show_main_menu():
         if option == '1':
                 playerName = input("Username: ")    
                 init_turn(turn, score, coins)
-                
-            
-
         elif option == '2':
                 ## Add in code to read save file     
                 print("Map loaded from text file successfully!")
@@ -160,8 +165,6 @@ def show_main_menu():
         elif option == '4':
                 print("Goodbye")
                 return False
-            
-    
         else:
             print("Invalid option. Please enter a valid choice.")
             return
@@ -175,11 +178,13 @@ def build(turn, buildingType):
      building.row = ord(position[0].upper()) - ord('A')
      if turn == 1:
          field[building.row][building.col][0] = buildingType
+         return buildingType, building.row, building.col
      else:
         index = letters.index(position[0].upper())
         check = validPosition(building.row, building.col)
         if check == True:
             field[building.row][building.col][0] = buildingType
+            return buildingType, building.row, building.col
         else:
             while check != True:
                 position = input("Enter the position: ")
@@ -187,63 +192,146 @@ def build(turn, buildingType):
                 building.row = ord(position[0].upper()) - ord('A')
                 check = validPosition(building.row, building.col)
             field[building.row][building.col][0] = buildingType
+            return buildingType, building.row, building.col
+        
+            
         
 
+def calculateScore(btype, row, col):
+    tempscore = 0
+    tempcoins = 0
+    indices_to_check = [
+            (row, col-1), (row, col+1),
+            (row-1, col), (row+1, col),
+            (row-1, col-1), (row-1, col+1),
+            (row+1, col-1), (row+1, col+1)
+        ]
+    if btype == "R":
+        temp = ["R","C"]
         
+        # Residential next to industry(I)
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        for dr, dc in directions:
+            try:
+                if field[row + dr][col + dc][0] == "I":
+                    tempscore += 1
+            except:
+                pass
+        # Residential adjacent to Residential(R) or Commercial(C)
+        for i, j in indices_to_check:
+            try:
+                if field[i][j][0] in temp:
+                    tempscore += 1
+                if field[i][j][0] == "C":
+                    tempcoins += 1
+            except:
+                pass
+        # Residential(R) adjacent to Park(O)
+        for i, j in indices_to_check:
+            try:
+                if field[i][j][0] == "O":
+                    tempscore += 2
+            except:
+                pass
+    
+    if btype == "I":
+        tempscore += 1
+        # Industry(I) adjacent to Residential(R)
+        for i, j in indices_to_check:
+            try:
+                if field[i][j][0] == "R":
+                    tempcoins += 1
+                    tempscore += 1
+            except:
+                pass
+            
+    if btype == "C":
+        # Commercial(C) adjacent to Commercial(C)
+        for i, j in indices_to_check:
+            try:
+                if field[i][j][0] == "C":
+                    tempscore += 1
+            except:
+                pass
+        # Commercial (C) adjacent to Residential(R)
+        for i, j in indices_to_check:
+            try:
+                if field[i][j][0] == "R":
+                    tempcoins += 1
+                    tempscore += 1
+            except:
+                pass
+
+    if btype == "O":
+        # Park(O) adjacent to Park(O)
+        for i, j in indices_to_check:
+            try:
+                if field[i][j][0] == "O":
+                    tempscore += 1
+            except:
+                pass
+        # Park(O) adjacent to Residential(R)
+        for i, j in indices_to_check:
+            try:
+                if field[i][j][0] == "R":
+                    tempscore += 2
+            except:
+                pass
+    
+    if btype == "*":
+        if field[row][col-1][0] == "*":
+            tempscore += 1
+        if field[row][col+1][0] == "*":
+            tempscore += 1
+            
+    return tempscore, tempcoins
 
     
 def validPosition(row, col):
     # A1
     if row == 0 and col == 0:
-        if field[row+1][col][0] != None or field[row][col+1][0] != None or field[row+1][col+1][0] != None:
+        if field[row+1][col][0] != None or field[row][col+1][0] != None: #or field[row+1][col+1][0] != None
             return True
         return False
     # A19
     if row == 0 and col == 19:
-        if field[row][col-1][0] != None or field[row+1][col][0] != None or field[row-1][col+1][0] != None:
+        if field[row][col-1][0] != None or field[row+1][col][0] != None: # or field[row-1][col+1][0] != None
             return True
         return False
     # T0
     if row == 19 and col == 0:
-        if field[row][col+1][0] != None or field[row-1][col][0] != None or field[row-1][col+1][0] != None:
+        if field[row][col+1][0] != None or field[row-1][col][0] != None: # or field[row-1][col+1][0] != None
             return True
         return False
     # T19
     if row == 19 and col == 19:
-        if field[row][col-1][0] != None or field[row-1][col][0] != None or field[row-1][col-1][0] != None:
+        if field[row][col-1][0] != None or field[row-1][col][0] != None: # or field[row-1][col-1][0] != None
             return True
         return False
     # Row A
     if row == 0:
-        if field[row][col+1][0] != None or field[row][col-1][0] != None or field[row+1][col][0] != None or field[row+1][col-1][0] != None or field[row+1][col+1][0] != None:
+        if field[row][col+1][0] != None or field[row][col-1][0] != None or field[row+1][col][0] != None: # or field[row+1][col-1][0] != None or field[row+1][col+1][0] != None
             return True
         return False
     # Row T
     if row == 19:
-        if field[row][col+1][0] != None or field[row][col-1][0] != None or field[row-1][col][0] != None or field[row-1][col-1][0] != None or field[row-1][col+1][0] != None:
+        if field[row][col+1][0] != None or field[row][col-1][0] != None or field[row-1][col][0] != None: # or field[row-1][col-1][0] != None or field[row-1][col+1][0] != None
             return True
         return False
     # Column 0
     if col == 0:
-        if field[row+1][col][0] != None or field[row-1][col][0] != None or field[row][col+1][0] != None or field[row-1][col-1][0] != None or field[row+1][col+1][0] != None:
+        if field[row+1][col][0] != None or field[row-1][col][0] != None or field[row][col+1][0] != None: # or field[row+1][col+1][0] != None or field[row-1][col-1][0] != None
             return True
         return False
     # Column 19
     if col == 0:
-        if field[row+1][col][0] != None or field[row-1][col][0] != None or field[row][col-1][0] != None or field[row-1][col-1][0] != None or field[row+1][col+1][0] != None:
+        if field[row+1][col][0] != None or field[row-1][col][0] != None or field[row][col-1][0]: # != None or field[row-1][col-1][0] != None or field[row+1][col+1][0] != None
             return True
         return False
     # The rest 
-    for i in range(row-1,row+2):
-        for x in range(col-1,col+2):
-            if field[i][x][0] != None:
-                return True
+    if field[row][col-1][0] != None or field[row][col+1][0] != None or field[row-1][col][0] != None or field[row+1][col][0] != None:
+        return True
     return False
-
-
-# (1,2)  (1,3)  (1,4)           
-# (2,2)  (2,3)  (2,4)  
-# (3,2)  (3,3)  (3,4)
 
 
 show_main_menu()
